6170e7f5709566bd6abb043f8a2fb6db17ec86e5
ManuT18 <manuturo2008@gmail.com>
2026-04-07 15:19:34 -0300

    Fix: Migrate Serverless Function to ES Modules to compat with Vite

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-const axios = require('axios');
+import express from 'express';
+import cors from 'cors';
+import axios from 'axios';
 
 const app = express();
 app.use(cors());
@@ -107,4 +107,4 @@ app.get(['/api/search', '/search', '/'], async (req, res) => {
     res.json(sortedBrands);
 });
 
-module.exports = app;
+export default app;
